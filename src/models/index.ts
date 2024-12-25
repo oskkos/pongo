623675b84681f2a7b2291ed21f3414e97bd32d27
Oskari Kosonen <oskari.kosonen@mylab.fi>
2024-12-25 17:26:16 +0200

    feat: :sparkles: Add Prisma integration
    Prisma integration with initial database schema and models for apartments and tenants.
    Also apartmentRepository stub implemented for reference.

---
+++ src/models/index.ts
@@ -0,0 +1,11 @@
+export interface Apartment {
+  id: string;
+  apartmentSize: number;
+  createdAt: Date;
+  description: string;
+  modifiedAt: Date;
+  postalCode: string;
+  postOffice: string;
+  streetAddress: string;
+  title: string;
+}
