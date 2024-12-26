1b4e8d657078c9ea662960738d5724a462afd799
Oskari Kosonen <oskari.kosonen@mylab.fi>
2024-12-26 22:12:48 +0200

    feat: :sparkles: Add next-cloudinary integration. Add Image component for optimized image delivery

---
+++ src/components/image.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { CldImage, CldImageProps } from 'next-cloudinary';
+import { MdHouse } from 'react-icons/md';
+
+// By default, the CldImage component applies auto-format and auto-quality to all delivery URLs for optimized delivery.
+export default function Image(props: CldImageProps) {
+  try {
+    const img = (
+      <CldImage
+        {...props}
+        onError={(e) => {
+          console.log(e);
+        }}
+      />
+    );
+    return img;
+  } catch (error) {
+    console.error(error);
+    return <MdHouse size={props.width} />;
+  }
+}
