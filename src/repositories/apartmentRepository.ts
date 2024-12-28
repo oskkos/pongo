7ee013a9b32c25144b7007fe5659a68eb71a8c35
Oskari Kosonen <oskari.kosonen@mylab.fi>
2024-12-28 21:27:28 +0200

    feat: :sparkles: Add apartment detail page with data fetching by slug

--- src/repositories/apartmentRepository.ts
+++ src/repositories/apartmentRepository.ts
@@ -5,9 +5,12 @@ import { prisma } from 'prisma/prisma';
 
 export function getAllApartments() {
   return prisma.apartment.findMany({
-    include: {
-      tenants: true,
-    },
+    orderBy: { streetAddress: 'asc' },
+  });
+}
+export function getApartmentBySlug(slug: string) {
+  return prisma.apartment.findUnique({
+    where: { slug },
   });
 }
 export function addNewApartment(data: Omit<Apartment, 'id' | 'createdAt' | 'modifiedAt'>) {
